005545daf0d92ef3ad9a4dd5c40c509ba340965d
Julian Jackson <julian.jackson@jaywing.com>
2018-10-08 13:32:10 +0100

    added ref to canvas, started draw function

--- the-dollop-scollop/src/Canvas.js
+++ the-dollop-scollop/src/Canvas.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React from 'react';
 
 class Canvas extends React.Component {
+  canvas = React.createRef();
+
+  draw = e => {
+    let ctx = this.canvas.current.getContext('2d');
+
+    if (e.nativeEvent.which === 1) {
+      console.log(ctx);
+    }
+  };
   render() {
     return (
       <canvas
+        onMouseDown={this.draw}
         width={this.props.canvasWidth}
         height={this.props.canvasHeight}
         className="Dollop-Scollop"
+        ref={this.canvas}
       />
     );
   }
