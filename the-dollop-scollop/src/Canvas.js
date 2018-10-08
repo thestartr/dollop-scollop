a3f238c871f1dbd25870516069c37c73b8d1d91f
isjulianJaywing <julian.jackson@jaywing.com>
2018-10-08 22:18:23 +0100

    Got mouse position

--- the-dollop-scollop/src/Canvas.js
+++ the-dollop-scollop/src/Canvas.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Rect, getMousePos } from "./tools/draw.js";
 
 class Canvas extends React.Component {
   canvas = React.createRef();
 
   draw = e => {
-    let ctx = this.canvas.current.getContext('2d');
+    let ctx = this.canvas.current.getContext("2d");
 
     if (e.nativeEvent.which === 1) {
-      console.log(ctx);
+      console.log(e.nativeEvent);
+      let clickPos = getMousePos(this.canvas.current, e.nativeEvent);
+      Rect(clickPos.x, clickPos.y, 2, 2, ctx);
     }
   };
   render() {
     return (
       <canvas
-        onMouseDown={this.draw}
+        onMouseMove={this.draw}
         width={this.props.canvasWidth}
         height={this.props.canvasHeight}
         className="Dollop-Scollop"
--- the-dollop-scollop/src/tools/draw.js
+++ the-dollop-scollop/src/tools/draw.js
@@ -1,11 +1,14 @@
 // Functions that help you draw to the canvas.
 
 //
-drawRect({x, y, width, height ) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
+export function Rect(x, y, width, height, ctx) {
+  ctx.fillRect(x, y, width, height);
+}
 
-
+export function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
 }
