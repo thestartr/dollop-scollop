6004e6e43f00a43ac616c3b3dba75250f2c48046
isjulianJaywing <julian.jackson@jaywing.com>
2018-10-10 22:40:13 +0100

    not working still

--- the-dollop-scollop/src/Canvas.js
+++ the-dollop-scollop/src/Canvas.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import { Rect, getMousePos, Pencil } from './tools/draw.js';
+import React from "react";
+import { Rect, getMousePos, Pencil } from "./tools/draw.js";
 
 class Canvas extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      prevX: '',
-      prevY: '',
-      x: '',
-      y: ''
+      prevX: "",
+      prevY: "",
+      x: "",
+      y: ""
     };
   }
 
   canvas = React.createRef();
 
-  draw = e => {
-    let ctx = this.canvas.current.getContext('2d');
-    let prevClickPos = getMousePos(this.canvas.current, e.nativeEvent);
-    console.log(this.state);
-    this.setState({
-      prevX: prevClickPos.x,
-      prevY: prevClickPos.y
-    });
-
-    // Rect(this.state.prevX, this.state.prevY, 1, 1, ctx);
-    Pencil(
-      this.state.prevX,
-      this.state.prevY,
-      prevClickPos.x,
-      prevClickPos.x,
-      '',
-      ctx
+  setCoords = e => {
+    let ctx = this.canvas.current.getContext("2d");
+    let mousePos = getMousePos(this.canvas.current, e.nativeEvent);
+    this.setState(
+      {
+        prevX: mousePos.x,
+        prevY: mousePos.y
+      },
+      () => console.log(this.state)
     );
+    // console.log(this.state);
+    Rect(this.state.prevX, this.state.prevY, 1, 1, ctx);
+  };
+
+  draw = e => {
+    if (e.nativeEvent.which !== 1) return;
+    let ctx = this.canvas.current.getContext("2d");
+    let mousePos = getMousePos(this.canvas.current, e.nativeEvent);
+
+    Pencil(this.state.prevX, this.state.prevY, mousePos.x, mousePos.x, "", ctx);
   };
   render() {
     return (
       <canvas
-        onMouseDown={this.draw}
+        onMouseDown={this.setCoords}
         onMouseMove={this.draw}
         width={this.props.canvasWidth}
         height={this.props.canvasHeight}
