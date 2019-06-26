4da90d5c2ba576d78152fad4235c73dfa5076602
Julian Jackson <julian.jackson@jaywing.com>
2019-06-26 17:07:45 +0100

    dollop scollop updates

--- dollopScollop/backend/public/javascripts/index.js
+++ dollopScollop/backend/public/javascripts/index.js
@@ -3,16 +3,28 @@
 
   const canvasContainer = document.querySelector('.c-dollopScollop__container');
   // const ctx = canvas.getContext('2d');
+  var socket = io();
   let isDrawing = false;
 
   // functions
   function createCanvas(parentContainer, canvasWidth, canvasHeight, canvasID) {
     this.canvas = document.createElement('canvas');
     this.canvas.setAttribute('id', canvasID);
-    this.canvas.setAttribute('width', this.canvasWidth || 500);
-    this.canvas.setAttribute('height', this.canvasHeight || 500);
+    this.canvas.setAttribute('width', 1000);
+    this.canvas.setAttribute('height', 500);
     parentContainer.appendChild(this.canvas);
+
     return this.canvas;
+  }
+
+  socket.on('mouse', newDrawing);
+  function newDrawing(data) {
+    ctx.strokeStyle = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.lineTo(data.x, data.y);
+    ctx.stroke();
+    ctx.closePath();
   }
 
   function getMousePos(canvas, evt) {
@@ -25,19 +37,29 @@
 
   function getCanvasPosition(e) {
     isDrawing = true;
-    ctx.lineWidth = 1;
+    ctx.lineWidth = 10;
+
     ctx.lineJoin = ctx.lineCap = 'round';
 
     let mouseCoords = getMousePos(canvas, e);
     ctx.moveTo(mouseCoords.x, mouseCoords.y);
   }
 
-  function drawLine(e) {
+  function mouseDragged(e) {
     let mouseCoords = getMousePos(canvas, e);
+
     if (isDrawing) {
+      let data = {
+        x: mouseCoords.x,
+        y: mouseCoords.y
+      };
+
+      socket.emit('mouse', data);
+      ctx.strokeStyle = '#000';
+      ctx.beginPath();
       ctx.lineTo(mouseCoords.x, mouseCoords.y);
       ctx.stroke();
-      console.log(ctx.getImageData(0, 0, canvas.width, canvas.height));
+      ctx.closePath();
     }
   }
 
@@ -46,14 +68,18 @@
   // create canvas & get context
   const canvas = new createCanvas(
     canvasContainer,
-    500,
-    500,
+    1000,
+    1000,
     'c-dollopScollp__canvas'
   );
   const ctx = canvas.getContext('2d');
 
   // event listeners
   canvas.addEventListener('mousedown', getCanvasPosition);
-  canvas.addEventListener('mousemove', drawLine);
+  canvas.addEventListener('mousemove', mouseDragged);
   canvas.addEventListener('mouseup', () => (isDrawing = false));
 })();
+
+//TODO: fill line gaps
+//TODO: allow selection of colour from user
+//TODO: set mouse position from socket users
